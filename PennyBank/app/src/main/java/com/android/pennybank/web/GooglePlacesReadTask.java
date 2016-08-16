7d9a290e61a6e060add35cc7425a787287518577
mkorunoski <mkorunoski@hotmail.com>
2016-08-16 15:33:54 +0200

    Implemented places search.

--- PennyBank/app/src/main/java/com/android/pennybank/web/GooglePlacesReadTask.java
+++ PennyBank/app/src/main/java/com/android/pennybank/web/GooglePlacesReadTask.java
@@ -1,34 +1,51 @@
 package com.android.pennybank.web;
 
 import android.os.AsyncTask;
-import android.util.Log;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class GooglePlacesReadTask extends AsyncTask<Object, Integer, String> {
+import org.json.JSONObject;
 
-    String googlePlacesData = null;
+import java.util.HashMap;
+import java.util.List;
+
+public class GooglePlacesReadTask extends AsyncTask<Object, Integer, List<HashMap<String, String>>> {
+
     GoogleMap googleMap;
 
     @Override
-    protected String doInBackground(Object... inputObj) {
+    protected List<HashMap<String, String>> doInBackground(Object... inputObj) {
+        String googlePlacesData;
+        JSONObject googlePlacesJson;
+        List<HashMap<String, String>> googlePlacesList = null;
+        Places placeJsonParser = new Places();
         try {
             googleMap = (GoogleMap) inputObj[0];
             String googlePlacesUrl = (String) inputObj[1];
             Http http = new Http();
             googlePlacesData = http.read(googlePlacesUrl);
+
+            googlePlacesJson = new JSONObject(googlePlacesData);
+            googlePlacesList = placeJsonParser.parse(googlePlacesJson);
         } catch (Exception e) {
-            Log.d("Google Place Read Task", e.toString());
+            e.printStackTrace();
         }
-        return googlePlacesData;
+
+        return googlePlacesList;
     }
 
     @Override
-    protected void onPostExecute(String result) {
-        Object[] toPass = new Object[2];
-        toPass[0] = googleMap;
-        toPass[1] = result;
-        (new PlacesDisplayTask()).execute(toPass);
+    protected void onPostExecute(List<HashMap<String, String>> list) {
+        for (int i = 0; i < list.size(); i++) {
+            HashMap<String, String> googlePlace = list.get(i);
+            double lat = Double.parseDouble(googlePlace.get("lat"));
+            double lng = Double.parseDouble(googlePlace.get("lng"));
+            String placeName = googlePlace.get("place_name");
+
+            googleMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title(placeName));
+        }
     }
 
 }
